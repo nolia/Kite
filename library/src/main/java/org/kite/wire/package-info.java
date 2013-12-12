9ada344059c2ec164eefe276f0b81e847fa1bb83
Nikolay Soroka <nolia4ta@gmail.com>
2013-12-12 05:50:31 +0200

    Package javadoc for wire.

--- library/src/main/java/org/kite/wire/package-info.java
+++ library/src/main/java/org/kite/wire/package-info.java
@@ -3,9 +3,51 @@
  * Connecting to local services ( that are in same process as their clients)
  * using binding mechanism, and by default will also fill dependencies in client.
  * <br/>
- * @see org.kite.wire.Wire
- * @see org.kite.wire.WiredService
+ * <pre>
+ public class MyService extends WiredService {
+    &#064;Provided
+    WorkerInterface worker = new WorkerImpl();
+ }
+
+ // it can be Activity, Fragment or any object
+ // what you need, is Context
+ public class MyClient {
+    &#064;Wired
+    WorkerInterface serviceWorker;
+
+    // init
+    wire = new Wire(context).from(MyService.class).to(this);
+
+    // onStart():
+    wire.connect();
+    // serviceWorker will be filled with WorkerImpl instance from
+    // service
+
+    // onStop():
+    wire.disconnect();
+    // serviceWorker will be nullified
+ }
+ * </pre>
+ * <p>Scopes</p>
+ * By default, when you connect pointing service class,
+ * you get all provided instances with ALL scope.
+ * So if you want to adhere encapsulation, you can set scope provided instances:
+ * <br/>
+ * <pre>
+     // will be available only when intent.action == SOME_INTENT_ACTION
+    &#064;Provided(scope = Scope.ACTION, action = SOME_INTENT_ACTION )
+    SomeInterface worker;
+
+    // will be available with any intent and with no intent
+    &#064;Provided(scope = Scope.ALL )
+    SomeInterface worker;
+
+    // default, will be available only when no intent is set in connection
+    &#064;Provided(scope = Scope.DEFAULT)
+    SomeInterface worker;
+
+ * </pre>
  *
- * @author Nikolay Soroka 
+ * @author Nikolay Soroka
  */
 package org.kite.wire;
